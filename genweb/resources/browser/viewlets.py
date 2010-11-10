bac90d0c95db586d9359ed2720a3c077198dc585
victor.fernandez <victor.fernandez@6deaf63b-a510-49cf-aad4-8c651776ecce>
2010-11-10 09:02:41 +0000

    Reintegració del branch de Plone 4.
    git-svn-id: https://devel.upcnet.es/svn/genwebupc/trunk/genweb.resources@2102 6deaf63b-a510-49cf-aad4-8c651776ecce

--- genweb/resources/browser/viewlets.py
+++ genweb/resources/browser/viewlets.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.app.layout.viewlets.common import ViewletBase
 from plone.app.layout.viewlets import common
